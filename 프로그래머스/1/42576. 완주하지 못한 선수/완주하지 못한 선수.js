f0dfbbdb9d803a4ecc4c4637e364530477620f31
김시우 <160982914+k-siwoo@users.noreply.github.com>
2024-09-19 20:04:52 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.97 ms, Memory: 72.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -0,0 +1,20 @@
+function solution(participant, completion) {
+    let hash = {};
+    for(let i of participant){
+        if(hash[i]) {
+            hash[i] += 1;
+        }
+        else{
+            hash[i] = 1;
+        }
+    }
+    for(let i of completion) {
+        if(hash[i] == 1){
+            delete hash[i];
+        }
+        else {
+            hash[i] -= 1;
+        }
+    }
+    return Object.keys(hash)[0];
+}
