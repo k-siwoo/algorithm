fb0dc5819ab28e8fdb7e70f9c119fc4fd387cf87
김시우 <ksw98035213@gmail.com>
2026-06-16 14:05:13 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 31.98 ms, Memory: 85.6 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -2,8 +2,7 @@ function solution(arr){
     let arr2 = [];
     arr2[0] = arr.shift();
     arr.forEach(num => {
-        let a = arr2[arr2.length - 1];
-        if(a != num){
+        if(arr2[arr2.length - 1] != num){
             arr2.push(num)
         }
     })    
